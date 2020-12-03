b1e8a87c29dbe05a914895388d2989989f53045c
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2020-12-03 14:20:42 -0500

    tables created for DB

---
+++ novels.sql
@@ -0,0 +1,44 @@
+CREATE DATABASE novels;
+
+use novels;
+
+CREATE TABLE authors(
+    id INT auto_increment,
+    name VARCHAR(255),
+    createdAt DATETIME DEFAULT NOW(),
+    updatedAt DATETIME DEFAULT NOW() ON UPDATE NOW(),
+    deletedAt DATETIME,
+    PRIMARY KEY(id)
+)
+
+CREATE TABLE novels(
+    id INT auto_increment,
+    name VARCHAR(255),
+    createdAt DATETIME DEFAULT NOW(),
+    updatedAt DATETIME DEFAULT NOW() ON UPDATE NOW(),
+    deletedAt DATETIME,
+    PRIMARY KEY(id),
+    FOREIGN KEY(authorId) REFERENCES authors(id)
+)
+
+CREATE TABLE genres(
+    id INT auto_increment,
+    name VARCHAR(255),
+    createdAt DATETIME DEFAULT NOW(),
+    updatedAt DATETIME DEFAULT NOW() ON UPDATE NOW(),
+    deletedAt DATETIME,
+    PRIMARY KEY(id)
+)
+
+CREATE TABLE novels_genres_link(
+    novelsId INT,
+    genresId INT,
+    name VARCHAR(255),
+    createdAt DATETIME DEFAULT NOW(),
+    updatedAt DATETIME DEFAULT NOW() ON UPDATE NOW(),
+    deletedAt DATETIME,
+    PRIMARY KEY(novelsID, genresId)
+    FOREIGN KEY(novelsId) REFERENCES novels(id),
+    FOREIGN KEY(genresId) REFERENCES genres(id)
+)
+
