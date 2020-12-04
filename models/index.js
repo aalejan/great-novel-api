3bf8318d138a8f6091127826e07b5271f219cea3
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2020-12-04 16:18:54 -0500

    models are setup, now we can make our routes and controllers

--- models/index.js
+++ models/index.js
@@ -7,4 +7,16 @@ const Novels_Genres_LinkModel = require('./novels_genres_link')
 const connection = new Sequelize('novels', 'novels', 'novels$$', {
     host: 'localhost', dialect: 'mysql'
   })
-  
+
+const Authors = AuthorsModel(connection, Sequelize)
+const Novels = NovelsModel(connection, Sequelize, Authors)
+const Genres = GenresModel(connection, Sequelize)
+const Novels_Genres_Link = Novels_Genres_LinkModel(connection, Sequelize, Novels, Genres)
+
+Authors.hasMany(Novels)
+Novels.belongsTo(Authors)
+
+Genres.belongsToMany(Novels, {through: Novels_Genres_Link})
+Novels.belongsToMany(Genres, {through: Novels_Genres_Link})
+
+module.exports = {Authors, Novels, Genres, Novels_Genres_Link}
