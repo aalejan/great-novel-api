0fd0d793e51e17fd9b9670586f63d4309cac51cc
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2020-12-04 11:34:40 -0500

    tables updated

--- novels.sql
+++ novels.sql
@@ -13,7 +13,8 @@ CREATE TABLE authors(
 
 CREATE TABLE novels(
     id INT auto_increment,
-    name VARCHAR(255),
+    title VARCHAR(255),
+    autorId INT,
     createdAt DATETIME DEFAULT NOW(),
     updatedAt DATETIME DEFAULT NOW() ON UPDATE NOW(),
     deletedAt DATETIME,
