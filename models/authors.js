43b923ff776e0591c8da0a411577ec5fb0111c11
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2020-12-04 15:35:21 -0500

    connection made to tables in DB

--- models/authors.js
+++ models/authors.js
@@ -1,9 +1,9 @@
-const authors = (connection, Sequelize) => {
+const Authors = (connection, Sequelize) => {
     return connection.define('authors', {
       id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
       name: { type: Sequelize.STRING },
     }, { paranoid: true })
   }
   
-  module.exports = authors
+  module.exports = Authors
   
