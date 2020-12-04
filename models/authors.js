bd6db38e017b92a14b3660b9af6b3fd1af07244a
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2020-12-04 15:14:11 -0500

    port created for server

---
+++ models/authors.js
@@ -0,0 +1,9 @@
+const authors = (connection, Sequelize) => {
+    return connection.define('authors', {
+      id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+      name: { type: Sequelize.STRING },
+    }, { paranoid: true })
+  }
+  
+  module.exports = authors
+  
