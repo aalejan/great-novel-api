30b68b41073051072614f06446eabf05b1455f66
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2020-12-05 10:52:20 -0500

    added controllers files

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const app = express()
+const{getAllAuthors, getAuthorById} = require('./controllers/authors')
+const{}
 
+app.get('/authors', getAllAuthors)
 
+app.get('/authors/:id', getAuthorById)
+
+app.get('/genres', getAllGenres)
+
+app.get('/genres/:id', getGenreById)
+
+app.get('/novels', getAllNovels)
+
+app.get('/novels/:id', getNovelById)
 
 
 const PORT = 3001
